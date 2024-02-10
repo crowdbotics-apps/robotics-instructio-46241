516733e119db136b0356c86cd64455c6f1cd2924
Crowdbotics <team@crowdbotics.com>
2024-02-10 23:36:19 +0000

    Initial commit

---
+++ backend/robotics_instructio_46241/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for robotics_instructio_46241 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "robotics_instructio_46241.settings"
+)
+
+application = get_wsgi_application()
